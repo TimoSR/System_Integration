1805dd74fb05dd64900eea0cd1bd4b1157a8bdc7
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-02-13 17:22:55 +0100

    Golang API

--- Lecture 2/golang_API/main.go
+++ Lecture 2/golang_API/main.go
@@ -10,5 +10,9 @@ func main() {
 		fmt.Fprint(w, "Hello, World!")
 	})
 
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "About page")
+	})
+
 	http.ListenAndServe(":8080", nil)
 }

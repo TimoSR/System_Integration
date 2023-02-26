b1927d266e9551c0271aa1f8ec43aba0069ac285
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-02-26 21:16:29 +0100

    small fixes

--- Lecture 2/Assignment/dotnet_project/dotnet_api/application/PersonFilesParser.cs
+++ Lecture 2/Assignment/dotnet_project/dotnet_api/application/PersonFilesParser.cs
@@ -39,51 +39,6 @@ namespace dotnet_api.application
         }
 
 
-        // public void readParseJsonFile()
-        // {
-
-        //     // System.Text.Json
-
-        //     string filepath = $"{fileFolder + jsonFilename}";
-
-        //     try
-        //     {
-        //         var cache = File.ReadAllText(filepath);
-        //         // converting the jsonString to a jsonObject
-        //         jsonContents = System.Text.Json.JsonSerializer.Deserialize<JsonElement>(cache);
-        //         // Creating a string for the console with formatting
-        //         var jsonString = System.Text.Json.JsonSerializer.Serialize(jsonContents, settings);
-        //         //Console.WriteLine(jsonString);
-        //         // // Testing if JsonObject was successfully created
-        //         name = jsonContents.GetProperty("name").GetString();
-        //         age = int.Parse(jsonContents.GetProperty("age").ToString());
-        //         email = jsonContents.GetProperty("email").GetString();
-        //         street = jsonContents.GetProperty("address").GetProperty("street").GetString();
-        //         city = jsonContents.GetProperty("address").GetProperty("city").GetString();
-        //         state = jsonContents.GetProperty("address").GetProperty("state").GetString();
-        //         zip = jsonContents.GetProperty("address").GetProperty("zip").GetString();
-
-        //         // Console.WriteLine(name);
-        //         // Console.WriteLine(age);
-        //         // Console.WriteLine(email);
-        //         // Console.WriteLine(street);
-        //         // Console.WriteLine(city);
-        //         // Console.WriteLine(state);
-        //         // Console.WriteLine(zip);
-
-        //     }
-        //     catch (FileNotFoundException ex)
-        //     {
-        //         Console.WriteLine($"The file could not be found: {ex.Message}");
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         Console.WriteLine($"An error occurred while reading the file: {ex.Message}");
-        //     }
-
-        // }
-
-
         private void readParseJsonFile()
         {
 
@@ -97,7 +52,7 @@ namespace dotnet_api.application
                 var person = JsonConvert.DeserializeObject<Person>(jsonString);
                 // Creating a good format for the console
                 var formatetJson = JsonConvert.SerializeObject(person, Formatting.Indented);
-                //Console.WriteLine(formatetJson);
+
                 _jsonContents = person;
 
                 // Setting getters
@@ -108,17 +63,6 @@ namespace dotnet_api.application
                 city = person.address.city;
                 state = person.address.state;
                 zip = person.address.zip;
-
-
-                // Checking if contents is accesble
-
-                // Console.WriteLine(name);
-                // Console.WriteLine(age);
-                // Console.WriteLine(email);
-                // Console.WriteLine(street);
-                // Console.WriteLine(city);
-                // Console.WriteLine(state);
-                // Console.WriteLine(zip);
 
             }
             catch (Exception err)

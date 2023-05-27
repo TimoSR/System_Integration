87f0ee485b40966e555873e1f6c21049736ea369
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-05-27 21:48:26 +0200

    Update

--- Lecture 9/SMS_Service/Program.cs
+++ Lecture 9/SMS_Service/Program.cs
@@ -1,12 +1,11 @@
-﻿
-// Load environment variables from .env file (if you're using DotNetEnv)
+﻿// Load environment variables from .env file (if you're using DotNetEnv)
 DotNetEnv.Env.Load();
 
 // Access the environment variables
 string servicePlanId = Environment.GetEnvironmentVariable("YOUR_servicePlanId");
 string apiToken = Environment.GetEnvironmentVariable("YOUR_API_token");
 string sinchVirtualNumber = Environment.GetEnvironmentVariable("YOUR_Sinch_virtual_number");
-string recipient_number = "+45205555";
+string recipient_number = Environment.GetEnvironmentVariable("YOUR_Recipient_Number");
 
 SMS sms = new SMS(sinchVirtualNumber, new string[] {recipient_number}, "Hello from Sinch!" );
 sms.sendSMS(sms, servicePlanId, apiToken);

e90ff058785a5ee74034a1cf85791298e3f14c1b
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-05-28 10:58:59 +0200

    Update

---
+++ Lecture 9/Email_Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+class Program
+{
+    static async Task Main()
+    {
+        DotNetEnv.Env.Load();
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var fromUser = "Timothy Rasmussen";
+        var fromEmailAddress = "Timothy.s.rasmussen@gmail.com";
+        var toEmailAddress = "Timothy.s.rasmussen@gmail.com";
+        var toUSer = "Timothy Rasmussen";
+        var msgTXT = "System Integration is fun and AMAZING!";
+
+        var client = new SendGridClient(apiKey);
+        var from = new EmailAddress(fromEmailAddress, fromUser);
+        var subject = "Proof of Working Email Service!";
+        var to = new EmailAddress(toEmailAddress, toUSer);
+        var plainTextContent = msgTXT;
+        var htmlContent = $"<strong>{msgTXT}</strong>";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
+    }
+}

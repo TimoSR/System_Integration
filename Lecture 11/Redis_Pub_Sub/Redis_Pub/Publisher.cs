09d2c4db28744fc6839ae249eb76cf4f9b989777
Archit3ct <timothy.s.rasmussen@gmail.com>
2023-05-29 22:59:38 +0200

    Update

---
+++ Lecture 11/Redis_Pub_Sub/Redis_Pub/Publisher.cs
@@ -0,0 +1,11 @@
+﻿using StackExchange.Redis;
+
+DotNetEnv.Env.Load();
+var redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+
+// Create a connection to Redis server
+var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+
+var pubSub = redis.GetSubscriber();
+
+pubSub.Publish("myChannel", "Hello, World!");
---
+++ Lecture 11/Redis_Pub_Sub/Redis_Sub/Subscriber.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+
+DotNetEnv.Env.Load();
+var redisConnectionString = Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING");
+
+// Create a connection to Redis server
+var redis = ConnectionMultiplexer.Connect(redisConnectionString);
+
+var pubSub = redis.GetSubscriber();
+
+pubSub.Subscribe("myChannel", (channel, message) =>
+{
+    // Print each message
+    Console.WriteLine(message);
+
+    // If the message is "quit", unsubscribe
+    if (message == "quit")
+    {
+        pubSub.Unsubscribe("myChannel");
+    }
+});
+
+// Wait for a key press to end the program
+Console.ReadKey();
